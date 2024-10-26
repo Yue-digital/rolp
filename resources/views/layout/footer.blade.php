bfb35b6cc6b1fbcd7a9952edf08aa56b0f007f45
MikkoAsahi <yuki031724@gmail.com>
2024-10-26 12:51:53 +0800

    frontpage

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,35 @@
+@section('footer')
+    <footer class="rolp-footer container-fluid">
+        <div class="row footer-row">
+            <div class="col-md-5 footer-address">
+                <h3>RESURRECTION OF OUR LORD PARISH</h3>
+                <p>E. Gaches corner J. Cabarrus Streets,</p>
+                <p>BF Homes, Parañaque City 1720, Philippines</p>
+            </div>
+
+            <div class="col-md-5 footer-contact">
+                <div class="footer-items">
+                    <h5>PARISH OFFICE</h5>
+                    <p>E. Gaches Street, BF Homes,</p>
+                    <p>Parañaque City 1720, Philippines</p>
+                    <p>Business Hours: 8:00 a.m. - noon; 2:00 - 6:00 p.m.</p>
+                    <p>(Tuesday to Sunday)</p>
+                </div>
+
+                <div class="footer-items">
+                    <h5>CONTACT US</h5>
+                    <p>+63 2 85192281 / 85192282</p>
+                    <p>inquiry@rolpbf.org / rolp1972@yahoo.com</p>
+                </div>
+
+
+            </div>
+        </div>
+
+        <div class="copy-row row">
+            <p>© 2024 RESURRECTION OF OUR LORD PARISH  - All Rights Reserved</p>
+            <p>BY Konbini Digital & ROLP SOCCOM</p>
+        </div>
+    </footer>
+
+@endsection
