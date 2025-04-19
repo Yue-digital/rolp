192ca035961188ae98764427af7cd7c16b633648
MikkoAsahi <kamikko0317@gmail.com>
2025-04-19 18:27:29 +0800

    .....

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -19,7 +19,7 @@
 
 <nav class="navbar navbar-expand-lg rolp-header-nav bg-transparent fixed-top ">
     <div class="container">
-        <a class="navbar-brand" href="#"><img src="{{asset('assets/img/LOGO.png')}}" alt=""></a>
+        <a class="navbar-brand" href="{{ url('/', []) }}"><img src="{{asset('assets/img/LOGO.png')}}" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
